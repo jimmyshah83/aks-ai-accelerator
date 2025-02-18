ea9a8695478f9906557cdf8be56baa0106feb07a
Jimmy Shah <jimmyshah83@gmail.com>
2025-02-18 09:14:08 -0500

    WIP: Serve

--- infrastructure/gpu-node-pool.sh
+++ infrastructure/gpu-node-pool.sh
@@ -5,9 +5,12 @@ RESOURCE_GROUP_NAME="rg-genai-accelerator-dev-cc-01"
 AKS_CLUSTER_NAME="genai-cluster-dev-cc-01"
 NODE_POOL_NAME="gpunp01"
 VM_SIZE="Standard_NC24s_v3"
-NODE_COUNT=4
+NODE_COUNT=1
+# VM_SIZE="Standard_NC96ads_A100_v4"
+# NODE_COUNT=2
 
-# Create spot node pool
+# To create spot node pool, use Spot as priority and enable eviction-policy
+#  --priority Spot \
 az aks nodepool add \
     --resource-group $RESOURCE_GROUP_NAME \
     --cluster-name $AKS_CLUSTER_NAME \
@@ -15,6 +18,5 @@ az aks nodepool add \
     --node-count $NODE_COUNT \
     --node-vm-size $VM_SIZE \
     --node-taints sku=gpu:NoSchedule \
-    --priority Spot \
     --labels purpose=llama-demo \
-    --eviction-policy Delete
+    # --eviction-policy Delete
