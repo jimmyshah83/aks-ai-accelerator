eae131ea21fa1c0cb4dc4561d9dbe6d5a6df0f4d
Jimmy Shah <jimmyshah83@gmail.com>
2025-03-15 11:08:34 -0400

    Run LLama 8B on A100 GPUs

--- infrastructure/ray-node-pool.sh
+++ infrastructure/ray-node-pool.sh
@@ -3,19 +3,15 @@
 # Variables
 RESOURCE_GROUP_NAME="rg-genai-accelerator-dev-cc-01"
 AKS_CLUSTER_NAME="genai-cluster-dev-cc-01"
-NODE_POOL_NAME="gpunp01"
-# VM_SIZE="Standard_NC24s_v3"
+NODE_POOL_NAME="raycpunp01"
+VM_SIZE="Standard_D16d_v5"
 NODE_COUNT=1
-VM_SIZE="Standard_NC96ads_A100_v4"
 
-# To create spot node pool, use Spot as priority and enable eviction-policy
-#  --priority Spot \
 az aks nodepool add \
     --resource-group $RESOURCE_GROUP_NAME \
     --cluster-name $AKS_CLUSTER_NAME \
     --name $NODE_POOL_NAME \
     --node-count $NODE_COUNT \
     --node-vm-size $VM_SIZE \
-    --node-taints sku=gpu:NoSchedule \
-    --labels purpose=llama-demo \
-    # --eviction-policy Delete
+    --node-taints sku=ray:NoSchedule \
+    --labels purpose=ray-head
