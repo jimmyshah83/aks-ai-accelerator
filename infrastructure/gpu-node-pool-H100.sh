60191889ad1869f2a961f2b4d3bfe9ec4ebb2914
Jimmy Shah <jimmyshah83@gmail.com>
2025-08-07 13:14:05 -0400

    Deploy gpt oss 120B to AKS A100 GPU - WIP

---
+++ infrastructure/gpu-node-pool-H100.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Variables
+RESOURCE_GROUP_NAME="rg-genai-accelerator-dev-cc-01"
+AKS_CLUSTER_NAME="genai-cluster-dev-cc-01"
+NODE_POOL_NAME="gpunp01"
+NODE_COUNT=1
+VM_SIZE="Standard_NC80adis_H100_v5"
+# VM_SIZE="Standard_NC24s_v3"
+
+# To disable spot node pool, remove Spot as priority and enable eviction-policy
+az aks nodepool add \
+    --resource-group $RESOURCE_GROUP_NAME \
+    --cluster-name $AKS_CLUSTER_NAME \
+    --name $NODE_POOL_NAME \
+    --node-count $NODE_COUNT \
+    --node-vm-size $VM_SIZE \
+    --node-taints sku=gpu:NoSchedule \
+    --labels purpose=vllm \
+    # --priority Spot \
+    # --eviction-policy Delete
